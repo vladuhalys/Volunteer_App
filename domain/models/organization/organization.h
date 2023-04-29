dc7e487743218c8852d647811f7ca823de274daf
vladuhalys <vlad.lysenko1999@gmail.com>
2023-04-29 20:05:30 +0300

    Micro update

--- domain/models/organization/organization.h
+++ domain/models/organization/organization.h
@@ -1,6 +1,15 @@
 #include "../volunteer/volunteer.h"
 
-class Organization : public 
+class Organization : public Account
 {
+	private:
+	string name;
+	string address;
+	string phone;
+	string email;
+	string website;
+	string description;
+	list<Volunteer> volunteers;
+
 };
 
