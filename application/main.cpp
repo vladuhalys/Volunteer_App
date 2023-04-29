3c12ba0487e8809b626660f23bec37a28702d32c
Vladyslav Lysenko <33177301+vladuhalys@users.noreply.github.com>
2023-04-29 18:06:26 +0300

    Update main.cpp

--- application/main.cpp
+++ application/main.cpp
@@ -1,14 +1,6 @@
-#include "../library.h"
-#include "../domain/models/person/person.h"
+#include <iostream>
 
 int main()
 {
-	Person person;
-	person.setName("Name");
-	person.setLastName("LastName");
-	person.setAge(18);
-	cout << person.getName() << endl;
-	cout << person.getLastName() << endl;
-	cout << person.getAge() << endl;
 	return 0;
 }
