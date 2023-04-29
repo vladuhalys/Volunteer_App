524229f120dc3899d21d272991888f0addef1805
Dzemc_bc16@student.itstep.org <Dzemc_bc16@student.itstep.org>
2023-04-29 15:51:21 +0300

    Update Roman #1

--- domain/models/client/client.cpp
+++ domain/models/client/client.cpp
@@ -7,3 +7,39 @@ Client::Client()
 Client::~Client()
 {
 }
+
+//Áåç ³äåé ùî ÿ òóò ğîáëş, ñêîğ³øå çà âñå ïğèéäåòüñÿ âèäàëèòè
+
+void Client::getDocument(string document)
+{
+	if (document.length() > 0)
+		this->document = document;
+	else
+		throw exception("No document found");
+}
+
+void Client::getInformationAboutRequest(string documentInfo)
+{
+	if (documentInfo.length() > 0)
+		this->documentInfo = documentInfo;
+	else
+		throw exception("No information about request found");
+}
+
+void Client::getVolunteerList(unsigned int id)
+{
+	this->id = id;
+}
+
+void Client::getCompanyList(unsigned int id)
+{
+	this->id = id;
+}
+
+void Client::getGoodsList(unsigned int id)
+{
+	this->id = id;
+}
+
+
+
