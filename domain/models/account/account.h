816b716b83d2be1df8366c00678ac991a0e938c6
Даша Побережник <dasosha2017@gmail.com>
2023-04-29 18:18:44 +0300

    ++

--- domain/models/account/account.h
+++ domain/models/account/account.h
@@ -1,5 +1,5 @@
 #include "../../../library.h"
-class Account
+class Account abstract
 {
 	private:
 	 uint32_t id;
@@ -9,37 +9,17 @@ class Account
 	 string phone_number;
 	public:
 	Account();
-	~Account();
-	void set_id(uint32_t id) {
-		this->id = id;
-	}
-	uint32_t get_id() {
-		return this->id;
-	}
-	void set_nickname(string nickname) {
-		this->nickname = nickname;
-	}
-	string get_nickname() {
-		return this->nickname;
-	}
-	void set_email(string email) {
-		this->email = email;
-	}
-	string get_email() {
-		return this->email;
-	}
-	void set_password(string password) {
-		this->password = password;
-	}
-	string get_password() {
-		return this->password;
-	}
-	void set_phone_number(string phone_number) {
-		this->phone_number = phone_number;
-	}
-	string get_phone_number() {
-		return this->phone_number;
-	}
+	virtual ~Account();
+	virtual void set_id(uint32_t id);
+	const uint32_t get_id();
+	virtual void set_nickname(string nickname);
+	const string get_nickname();
+	virtual void set_email(string email);
+	const string get_email();
+	virtual void set_password(string password);
+	const string get_password();
+	virtual void set_phone_number(string phone_number);
+	const string get_phone_number();
 	
 };
 
