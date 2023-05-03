7dac684bcc913d8bf50ecf4b2996752a9d67c963
fzyier <babeckiy2004@gmail.com>
2023-05-03 18:57:50 +0300

    Started working with classes Show

---
+++ presentation/ShowInformation/ShowCompanies/ShowCompanies.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../../../library.h"
+
+class ShowCompanies
+{
+public:
+// Fields
+// Constructors
+// Methods
+//        Getters
+//        Setters
+//        Other
+private:
+// Fields
+// Methods
+};
