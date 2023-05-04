a0be301ff241194dc100336aeab9ad724310b83f
fzyier <babeckiy2004@gmail.com>
2023-05-04 18:02:24 +0300

    * Added #prama once to account.h * Created and half-filled methods (accept/send/confirm)_goods * Changed Buy.h * Other...

--- domain/models/volunteer/volunteer.h
+++ domain/models/volunteer/volunteer.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma once
 
 #include "../person/person.h"
 #include "../goods/Goods.h"
@@ -13,6 +14,23 @@ public:
     Volunteer(const Volunteer& other);
     Volunteer(Volunteer&& other) noexcept;
     ~Volunteer();
+// Operators
+    Volunteer& operator=(Volunteer& other) = default;
+    Volunteer& operator=(Volunteer&& other) = default;
+// Methods
+// Getters
+    vector<Goods>& get_goods();
+    list<Statement>& get_statements();
+// Setters
+// Other
+    void accept_goods(Goods& goods);
+    void send_goods(const size_t& id);
+    void confirm_goods(Goods& goods);
+private:
+// Fields
+    vector<Goods> _goods;
+    list<Statement> _statements;
+// Methods
 // Operators
     Volunteer& operator=(Volunteer& other) = default;
     Volunteer& operator=(Volunteer&& other) = default;
--- presentation/ShowInformation/ShowInformation.h
+++ presentation/ShowInformation/ShowInformation.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../../library.h"
 #include "../../domain/models/company/company.h"
+#include "../../domain/models/volunteer/volunteer.h"
+#include "../../domain/models/company/company.h"
 #include "../../domain/models/organization/organization.h"
 #include "../../domain/models/volunteer/volunteer.h"
 
@@ -23,6 +25,49 @@ static vector<Company>& get_companies(list<Company>& companies)
 }
 //        Setters
 //        Other
+    static void show_company()
+    {
+        for (const Company& i : companies_) 
+        {
+            cout << i.getName() << '\n';
+            cout << i.getType() << '\n';
+            cout << i.getTerminDelivery() << '\n';
+        }
+    }
+    
+    // TODO: Take Organization in method's arguments and do double iteration from list of volunteers in organization and volunteer's goods.
+    static void show_purchases(Volunteer& _volunteer)
+    {
+        for (const Goods& i : _volunteer.get_goods()) 
+        {
+            cout << i.getName() << '\n';
+            cout << i.getPrice() << '\n';
+            cout << i.getDescription() << '\n';
+            cout << i.getQuantity() << '\n';
+            cout << i.getId() << '\n';
+            cout << i.getCategory_id() << '\n';
+            cout << '\n';
+        }
+
+    }
+
+    // TODO: Take Organization in method's arguments and do double iteration from list of volunteers in organization and volunteer's goods.
+    static void show_costs(Volunteer& _volunteer)
+    {
+        double num = 0;
+        for (const Goods& i : _volunteer.get_goods()) 
+        {
+            num += i.getPrice();
+        }
+
+        cout << num << '\n';
+    }
+
+    // TODO: Take Clients or Organization in method's argument
+    static void show_endUsers()
+    {
+        cout << "None" << '\n';
+    }
     static void show_company()
     {
         for (const Company& i : companies_) 
