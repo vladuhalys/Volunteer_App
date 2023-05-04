f7aec03e74417118740706167a832847337629be
fzyier <babeckiy2004@gmail.com>
2023-05-04 18:02:24 +0300

    * Added #prama once to account.h * Created and half-filled methods (accept/send/confirm)_goods * Changed Buy.h * Other...

--- domain/models/account/account.h
+++ domain/models/account/account.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "../../../library.h"
 class Account abstract
 {
--- domain/usecases/Send/Send.h
+++ domain/usecases/Send/Send.h
@@ -19,12 +19,12 @@ public:
         std::tm tm = *std::localtime(&t);
         std::string date_time = std::asctime(&tm);
         
-        string description = "Send " + volunteer_._goods.at(id).getName() + " " + to_string(volunteer_._goods.at(id).getId())
-        + to_string(volunteer_._goods.at(id).getCategory_id()) + to_string(volunteer_._goods.at(id).getQuantity()) + 
-        to_string(volunteer_._goods.at(id).getPrice()) + volunteer_._goods.at(id).getDescription();
+        string description = "Send " + volunteer_.get_goods().at(id).getName() + " " + to_string(volunteer_.get_goods().at(id).getId())
+        + to_string(volunteer_.get_goods().at(id).getCategory_id()) + to_string(volunteer_.get_goods().at(id).getQuantity()) + 
+        to_string(volunteer_.get_goods().at(id).getPrice()) + volunteer_.get_goods().at(id).getDescription();
 
-        volunteer_._statements.push_back(Statement(date_time,description,false,0));
-        volunteer_._goods.erase(volunteer_._goods.begin() + id);
+        volunteer_.get_statements().push_back(Statement(date_time,description,false,0));
+        //volunteer_.get_goods().erase(volunteer_.get_goods().begin() + id);
     }
 private:
 // Fields
