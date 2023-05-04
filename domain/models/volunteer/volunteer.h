55a15d30309dc74e3afb4a4a3a4d7210fb9aff55
fzyier <babeckiy2004@gmail.com>
2023-05-04 18:02:24 +0300

    * Added #prama once to account.h * Created and half-filled methods (accept/send/confirm)_goods * Changed Buy.h * Other...

--- domain/models/volunteer/volunteer.h
+++ domain/models/volunteer/volunteer.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "../person/person.h"
 #include "../goods/Goods.h"
 #include "../statement/Statement.h"
@@ -6,13 +7,26 @@ class Volunteer : public Person
 {
 public:
 // Fields
-    vector<Goods> _goods;
-    list<Statement> _statements;
 // Constructors
     Volunteer();
     Volunteer(const Volunteer& other);
     Volunteer(Volunteer&& other) noexcept;
     ~Volunteer();
-
-
+// Operators
+    Volunteer& operator=(Volunteer& other) = default;
+    Volunteer& operator=(Volunteer&& other) = default;
+// Methods
+// Getters
+    vector<Goods>& get_goods();
+    list<Statement>& get_statements();
+// Setters
+// Other
+    void accept_goods(Goods& goods);
+    void send_goods(const size_t& id);
+    void confirm_goods(Goods& goods);
+private:
+// Fields
+    vector<Goods> _goods;
+    list<Statement> _statements;
+// Methods
 };
