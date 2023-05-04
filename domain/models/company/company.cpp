a0e5773358bfb0df733bdcf77004c6362b89dfac
Sasha Onyshchenko <bigoleksandr@gmail.com>
2023-05-04 19:03:46 +0300

    update class product and class cart

--- domain/models/company/company.cpp
+++ domain/models/company/company.cpp
@@ -1,6 +1,6 @@
 #include "company.h"
 
-// constructor
+// constructor Company
 
 Company::Company(string name_, string type_, int terminDelivery_) {
     name = name_;
@@ -34,4 +34,80 @@ void Company::setType(string type_) {
 
 void Company::setTerminDelivery(int terminDelivery_) {
     terminDelivery = terminDelivery_;
+}
+
+
+// constructor Product
+
+Product::Product(string name_, string description_, double price_, int quantity_) {
+    name = name_;
+    description = description_;
+    price = price_;
+    quantity = quantity_;
+}
+
+// getters
+
+string getName() const {
+    return name;
+}
+
+string getDescription() const {
+    return description;
+}
+
+double getPrice() const {
+    return price;
+}
+
+int getQuantity() const {
+    return quantity;
+}
+
+// setters
+
+void setName(string name_) {
+    name = name_;
+}
+
+void setDescription(string description_) {
+    description = description_;
+}
+
+void setPrice(double price_) {
+    price = price_;
+}
+
+void setQuantity(int quantity_) {
+    quantity = quantity_;
+}
+
+// constructor Cart
+
+Cart::Cart() {
+
+    vector<Product> products;
+}
+void addProduct(Product product) {
+    products.push_back(product);
+}
+
+void removeProduct(int index) {
+    products.erase(products.begin() + index);
+}
+
+void clearCart() {
+    products.clear();
+}
+
+double getTotalPrice() const {
+    double totalPrice = 0;
+    for (const auto& product : products) {
+        totalPrice += product.getPrice();
+    }
+    return totalPrice;
+}
+
+vector<Product> getProducts() const {
+    return products;
 }
