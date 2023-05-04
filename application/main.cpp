8759dcb1bdbcd0dbd1626ab48de8ed3b9354fbf4
Даша Побережник <dasosha2017@gmail.com>
2023-05-04 18:30:33 +0300

    +

--- application/main.cpp
+++ application/main.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+	SignIn* s = new SignIn("login", "password");
+    cout << s->getLogin() << endl;
+    cout << s->getPassword() << endl;
+    s->setLogin("login2");
+    s->setPassword("password2");
+    cout << s->getLogin() << endl;
+    cout << s->getPassword() << endl;
 	return 0;
 }
